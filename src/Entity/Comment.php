2323f1ac6e2a86cdb8c2fef60bd0b53f0a735446
ChristopheVanDooren <christophe.vandooren@icapps.com>
2023-11-09 09:23:38 +0100

    GUES_0012: async

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -27,7 +27,7 @@ class Comment
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
     #[Assert\Email]
-private ?string $email = null;
+    private ?string $email = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
@@ -38,6 +38,9 @@ private ?string $email = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $photoFilename = null;
+
+    #[ORM\Column(length: 255, options: ['default' => 'submitted'])]
+    private ?string $state = 'submitted';
 
     public function getId(): ?int
     {
@@ -118,6 +121,18 @@ private ?string $email = null;
     public function setPhotoFilename(?string $photoFilename): static
     {
         $this->photoFilename = $photoFilename;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(?string $state): static
+    {
+        $this->state = $state;
 
         return $this;
     }
